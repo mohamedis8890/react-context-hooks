a4b968559f9e997f2081da6237bcb102b440f0a0
Mo <mohamed.is8890@gmail.com>
2019-12-04 06:04:15 +0200

    Created and consumed ThemeContext, the static way

--- contextapp/src/App.js
+++ contextapp/src/App.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import BookList from './components/BookList';
-import Navbar from './components/Navbar';
+import React from "react";
+import BookList from "./components/BookList";
+import Navbar from "./components/Navbar";
+import ThemeContextProvider from "./contexts/ThemeContext";
 
 function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <BookList />
-    </div>
-  );
+	return (
+		<div className="App">
+			<ThemeContextProvider>
+				<Navbar />
+				<BookList />
+			</ThemeContextProvider>
+		</div>
+	);
 }
 
 export default App;
